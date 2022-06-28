523e0b8afe93ac53b91bda0f4ccdfb5ff12f1016
Sky <2850132896@qq.com>
2022-06-28 17:54:05 +0800

    ''

---
+++ src/Test.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Rainbows\Framework;
+
+class Test
+{
+    public static function test($dir){
+
+        $runtime = rtrim($dir, DIRECTORY_SEPARATOR).DIRECTORY_SEPARATOR.'runtime';
+
+        if(!is_dir($runtime))
+            mkdir($runtime, 0777, true);
+
+
+        file_put_contents($runtime.'/1.log', __METHOD__);
+
+        var_dump(__METHOD__);
+
+    }
+}
---
+++ tests/index.php
@@ -0,0 +1,5 @@
+<?php
+
+include_once '../vendor/autoload.php';
+
+\Rainbows\Framework\Test::test(__DIR__);
